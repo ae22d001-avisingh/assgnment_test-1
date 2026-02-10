a1e3d58b7880c8c4488bc4b1289bcc803053ad02
ae22d001-avisingh <ae22d001@smail.iitm.ac.in>
2026-02-10 07:03:57 +0000

    final_final

--- main.cpp
+++ main.cpp
@@ -1,26 +1,13 @@
 #include <fstream>
-#include <iostream>
 
 int main()
 {
     // write your code here
-    int len_seq{};
-
-    std::cin >> len_seq;
-
-    int fib_seq[len_seq];
-
-    fib_seq[0] = 0;
-    fib_seq[1] = 1;
-
-    for(int i = 2; i < len_seq; ++i)
-    {
-        fib_seq[i] = fib_seq[i - 1] + fib_seq[i - 2];
-    }
 
 
     // make sure that the resultant fibonacci sequence is stored in 'fib_seq' array
     // and the length of the array is stored in 'len_seq' variable
+    // so that the Output.txt file can be generated easily
 
     // #####################################################################
     // writing the results to a .dat file(dont change this part of the code)
--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 # write your code here
 # make sure that the resultant fibonacci sequence is stored in 'fib_seq' array
 # and the length of the array is stored in 'len_seq' variable
+# so that the Output.txt file can be generated easily
 
-fib_seq = [0, 1]
-len_seq = int(input('enter length'))
 
-for i in range(2, len_seq):
-    next = fib_seq[i - 1] + fib_seq[i - 2]
-    fib_seq.append(next)
-
-print(fib_seq)
 # #####################################################################
 # writing the results to a .dat file(dont change this part of the code)
 # #####################################################################
